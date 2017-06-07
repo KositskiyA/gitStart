004a459ff188ee64a0af19aaf9df41109443403b
KositskiyA <kositskiy.a@gmail.com>
2017-06-07 18:20:09 +0300

    add new game

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.Pause_Batton = new System.Windows.Forms.Button();
+            this.Stop_Button = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(29, 43);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "btn1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(29, 89);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "btn2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(29, 138);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "btn3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.Location = new System.Drawing.Point(56, 46);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(75, 23);
+            this.Start_Button.TabIndex = 3;
+            this.Start_Button.Text = "Start";
+            this.Start_Button.UseVisualStyleBackColor = true;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Pause_Batton
+            // 
+            this.Pause_Batton.Location = new System.Drawing.Point(162, 46);
+            this.Pause_Batton.Name = "Pause_Batton";
+            this.Pause_Batton.Size = new System.Drawing.Size(75, 23);
+            this.Pause_Batton.TabIndex = 4;
+            this.Pause_Batton.Text = "Pause";
+            this.Pause_Batton.UseVisualStyleBackColor = true;
+            this.Pause_Batton.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Stop_Button
+            // 
+            this.Stop_Button.Location = new System.Drawing.Point(275, 46);
+            this.Stop_Button.Name = "Stop_Button";
+            this.Stop_Button.Size = new System.Drawing.Size(75, 23);
+            this.Stop_Button.TabIndex = 5;
+            this.Stop_Button.Text = "Stop";
+            this.Stop_Button.UseVisualStyleBackColor = true;
+            this.Stop_Button.Click += new System.EventHandler(this.Stope_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Stop_Button);
+            this.groupBox1.Controls.Add(this.Start_Button);
+            this.groupBox1.Controls.Add(this.Pause_Batton);
+            this.groupBox1.Location = new System.Drawing.Point(324, 305);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(642, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 435);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.Button Pause_Batton;
+        private System.Windows.Forms.Button Stop_Button;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+
+
+namespace WindowsFormsApp2
+{
+    delegate void HelpMovment(Button btn);
+    public partial class MainForm : Form
+    {
+        static Random R;
+        HelpMovment HM;
+        Button[] massButton;
+        List<Button> listButtons;
+        Thread[] massThread;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            R = new Random();
+            HM = new HelpMovment(Movment);
+            massButton = new Button[3];
+            massButton[0] = btn1;
+            massButton[1] = btn2;
+            massButton[2] = btn3;
+
+            listButtons = new List<Button>();
+            listButtons.AddRange(massButton);
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (massThread != null)
+                ThreadResume(massThread);
+            else
+                ThreadInit();
+            Start_Button.Enabled = false;
+        }
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            ThreadSuspend(massThread);
+            Start_Button.Enabled = true;
+        }
+        private void Stope_Click(object sender, EventArgs e)
+        {
+            Pause_Click(sender, e);
+            DefaultButtonParams(massButton);
+        }
+
+        void DefaultButtonParams(params Button[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i].Location = new Point(25, list[i].Location.Y);
+                list[i].BackColor = Color.White;
+            }
+        }
+
+        void MoveButtonMethod1()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                Invoke(HM, btn1);
+            }
+        }
+        void MoveButtonMethod2()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                Invoke(HM, btn2);
+            }
+        }
+        void MoveButtonMethod3()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                Invoke(HM, btn3);
+            }
+        }
+
+        void Movment(Button btn)
+        {
+            btn.Location = new Point(btn.Location.X + R.Next(0, 30), btn.Location.Y);
+            var query = from buttonNomber in listButtons
+                        orderby buttonNomber.Location.X
+                        select buttonNomber;
+
+            massButton = query.ToArray();
+            SetColorForWiner(Color.Yellow, massButton);
+            Finish(btn);
+        }
+
+        private void Finish(Button btn)
+        {
+            if (btn.Location.X > (pictureBox1.Location.X))
+            {
+                this.Pause_Click(new object(), new EventArgs());
+                MessageBox.Show("Выиграла кнопка "+btn.Text);
+                Start_Button.Enabled = false;
+            }
+        }
+
+        void SetColorForWiner(Color color, params Button[] list)
+        {
+            list[list.Length - 1].BackColor = color;
+            for (int i = 0; i < list.Length - 1; i++)
+            {
+                list[i].BackColor = Color.White;
+            }
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Stope_Click(sender, e);
+        }
+        void ThreadInit()
+        {
+            massThread = new Thread[3];
+            massThread[0] = new Thread(MoveButtonMethod1);
+            massThread[1] = new Thread(MoveButtonMethod2);
+            massThread[2] = new Thread(MoveButtonMethod3);
+
+            massThread[0].Start();
+            massThread[1].Start();
+            massThread[2].Start();
+        }
+        void ThreadResume(Thread[] mass)
+        {
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i].Resume();
+            }
+        }
+        void ThreadSuspend(Thread[] mass)
+        {
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i].Suspend();
+            }
+        }
+    }
+}
